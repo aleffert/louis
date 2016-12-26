b92cdcd11aed9015dcb173d13eee761986382bd8
Akiva Leffert <aleffert@gmail.com>
2016-12-26 14:46:04 -0500

    Add some extra comments to LUIReport.h

--- Sources/LUIReport.h
+++ Sources/LUIReport.h
@@ -13,10 +13,17 @@ NS_ASSUME_NONNULL_BEGIN
 
 @protocol LUIReport <NSObject>
 
+/// Return all reports for this view.
 + (NSArray<id<LUIReport>>*)reports:(UIView*)view;
+
+/// A unique identifier for the class of reports. Used for filtering out reports for
+/// specific views
 + (NSString*)identifier;
 
+/// A user facing string describing the error
 @property (readonly, copy, nonatomic) NSString* message;
+
+/// A set of relevant views and user facing string describing each view's rule in the report
 @property (readonly, copy, nonatomic) NSDictionary<NSString*, UIView*>* views;
 
 @end
